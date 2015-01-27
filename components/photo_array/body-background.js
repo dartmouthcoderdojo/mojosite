c0d97d94fa07a6f7e2a43411958e5cae24a1310d
Julien Blanchet <j55blanchet@gmail.com>
2015-01-26 19:17:57 -0500

    basic site structure created

--- components/photo_array/body-background.js
+++ components/photo_array/body-background.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     var imgChoices = 6;
     var imgNum = Math.ceil(Math.random() * imgChoices);
     
-    document.body.style.backgroundImage = "url('/components/photo_array/" + imgNum + ".jpg')";
-    
+    document.body.style.backgroundImage = "url('/mojosite/components/photo_array/" + imgNum + ".jpg')";
 });
 
